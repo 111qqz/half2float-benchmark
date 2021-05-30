14856574308c7c2b1a2f449188e0a82a9b36caa1
renkuanze <renkuanze@sensetime.com>
2021-05-30 17:07:40 +0800

    update

--- BUILD
+++ BUILD
@@ -1,7 +1,7 @@
 load("@rules_cc//cc:defs.bzl", "cc_binary","cc_library")
 cc_library(
     name = "half",
-    srcs = ["half/half.h"]
+    srcs = ["half/half.hpp"]
 )
 cc_binary(
     name = "benchmark",
--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <cstdio>
 #include <iostream>
-#include "half/half.h"
+#include "half/half.hpp"
 #include <vector>
+#include <chrono>
 using namespace std;
 using half_float::half;
 using namespace half_float::literal;
@@ -14,10 +15,21 @@ c += 3;
 if(c > a)
     std::cout << c << std::endl;
 
-const int num = 55555;
+const int num = 12345;
 std::vector<half>halfs(num,234.5_h);
 std::vector<float>floats;
-
+floats.reserve(num);
+ auto t1 = std::chrono::high_resolution_clock::now();
+// floats.assign(halfs.begin(),halfs.end());
+for ( int i = 0 ; i < num ; i++){
+	floats[i] = static_cast<float>(halfs[i]);
+}
+ auto t2 = std::chrono::high_resolution_clock::now();
+ std::chrono::duration<double, std::nano> fp_ns = t2 - t1;
+ cout<<" takes "<<fp_ns.count()<<" ns"<<endl;
+// for (auto val:floats){
+// 	cout<<"val:"<<val<<endl;
+// }
 
 	return 0;
 }
